95a88402c3e500f0bdbfa7319ceed2588a6fda58
Lucía Balsa <lbalsamartinez@danielcastelao.org>
2022-10-21 21:53:23 +0200

    version 1.0 Ejercicio2

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // version 1.0
+        int lado = 3, area;
+        area = (int) Math.pow(lado, 2);
+        System.out.println("el area=" + area);
+    }
+}
